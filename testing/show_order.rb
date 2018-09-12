0592ac810edd926722c87c317da5a525f461cc4c
Glyn Hudson <glyn.hudson@gmail.com>
2018-09-12 13:29:51 +0000

    add starting bash script

--- testing/show_order.rb
+++ testing/show_order.rb
@@ -1,21 +1,26 @@
-#!/usr/bin/ruby
-
 require 'bigcommerce'
+require 'csv'
 
-Bigcommerce.configure do |config|
-  config.auth = 'legacy'
-  config.url = ENV['BC_API_ENDPOINT_LEGACY']
-  config.username = ENV['BC_USERNAME']
-  config.api_key = ENV['BC_API_KEY']
+
+api = Bigcommerce::Api.new({
+                             :store_url => "https://store-98a75.mybigcommerce.com/api/v2/",
+                             :username  => "mobile_ynyr_edwards_gmail_com",
+                             :api_key   => "a568318f22dbb55d7c9bd6cd092b101666840fdd01f89a3285"
+                           })
+
+
+@shop_orders = []
+
+@shop_orders <<  api.get_order(ARGV[0].to_i)
+
+puts "Downloading..."
+
+puts @shop_orders
+
+@shop_orders.each do |order|
+  puts "total_ex_tax:"
+  puts order["total_ex_tax"]
 end
-
-
-@order = Bigcommerce::Order.all[0]
-@order.id = ARGV[0].to_i
-
-# List order shipping address
-@order_shipping_addresses = Bigcommerce::OrderShippingAddress.all(@order.id)
-puts @order_shipping_addresses
 
 
 puts "Done!"
